9cbfffe56f4a5b6b81172e25c6f9e0e168de9b93
Francis <francois.billon@ensta-bretagne.org>
2022-02-21 14:39:27 +0100

    Hand

--- src/Cards/Hand.java
+++ src/Cards/Hand.java
@@ -1,4 +1,46 @@
 package Cards;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hand {
+
+    private final List<ICard> cards;
+
+    public Hand(ArrayList<ICard> hand){
+        this.cards = hand;
+    }
+
+    public void mergeHand(Hand other){
+        /**
+         * Fusionne la main actuelle avec d'autres cartes
+         */
+        for (ICard c : other.cards){
+            this.addCardToBottom(c);
+        }
+    }
+
+    public void addCardToBottom(ICard c) {
+        /**
+         *  Ajoute un carte en bas de la main
+         */
+        cards.add(0, c);
+    }
+
+    public ICard removeCardFromTop() {
+        /**
+         * Suprime la premiere carte de la main
+         */
+        if (handSize() < 1) {
+            return null;
+        }
+        return cards.remove(handSize() - 1);
+    }
+
+    public int handSize() {
+        /**
+         * Taille de la main
+         */
+        return cards.size();
+    }
 }
